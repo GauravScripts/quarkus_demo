32b377d9b84c3d05b7341b5136fa607d526b50d6
Gaurav Gupta <alwar.gaurav1@gmail.com>
2024-03-25 17:51:08 +0530

    Quarkus Demo Application

--- src/main/java/com/example/ExampleResource.java
+++ src/main/java/com/example/ExampleResource.java
@@ -1,16 +1,33 @@
 package com.example;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/hello")
 public class ExampleResource {
 
+    private Map<String, String> names = new HashMap<>();
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public String hello(@QueryParam("name") String name) {
+        String value = names.get(name);
+        if (value != null) {
+            return "Hello from Quarkus, " + value + "!";
+        } else {
+            return "No value found for " + name;
+        }
     }
+
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloPost(String name) {
+        names.put(name, name);
+        return "Hello from Quarkus, " + name + "!";
+    }
+
 }
